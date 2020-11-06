1b36ff4d9c45f8a591c6f57c7b9ed3d635174415
whattosay <porwal.deepak2000@gmail.com>
2020-11-06 19:06:58 +0530

    another gmail account

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCsb9PtPCnTvcbnXNRmJ0K0I49OJ2RnXAs",
-  authDomain: "mern-facebookclone.firebaseapp.com",
-  databaseURL: "https://mern-facebookclone.firebaseio.com",
-  projectId: "mern-facebookclone",
-  storageBucket: "mern-facebookclone.appspot.com",
-  messagingSenderId: "804261269369",
-  appId: "1:804261269369:web:b4ee9c0e82aa0d3ecfe9a8",
+  apiKey: "AIzaSyDKaf6jyjH5hPb47-KZUlxZSYVMaDcJNZg",
+  authDomain: "facebookclone-b0437.firebaseapp.com",
+  databaseURL: "https://facebookclone-b0437.firebaseio.com",
+  projectId: "facebookclone-b0437",
+  storageBucket: "facebookclone-b0437.appspot.com",
+  messagingSenderId: "349701584909",
+  appId: "1:349701584909:web:5e15ec19e3ade373853d44",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
